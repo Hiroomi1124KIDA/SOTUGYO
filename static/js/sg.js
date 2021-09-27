32af76afd2432653aaeba5c88fdaf26770ccb16d
hasuda <hagio1278@gmail.com>
2021-09-27 16:11:59 +0900

    FAQ部分追加

--- static/js/sg.js
+++ static/js/sg.js
@@ -1,7 +1,32 @@
 $(function () {
 
-  $(document).ready(function(){
+  $(document).ready(function () {
     $('.collapsible').collapsible();
+  });
+
+});
+
+
+
+
+$(function () {
+
+  $('.faq-list-item').click(function () {
+    var $answer = $(this).find('.answer');
+    if ($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      $answer.slideUp();
+
+      $(this).find('span').text('+');
+
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown();
+
+      $(this).find('span').text('-');
+
+    }
+
   });
 
 });
